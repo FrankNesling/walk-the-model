a401f2ab4898efbba47f4fa2c6ad845be812e2b1
Frank Nesling <matthias.gnilsen@hotmail.com>
2025-08-31 21:30:32 +0200

    walk the model - copy paste from learnopengl

--- Walk the Model/main.cpp
+++ Walk the Model/main.cpp
@@ -2,26 +2,27 @@
 #include <GLFW/glfw3.h>
 #include <helper.h>
 
-int camera();
+int walkthemodel(std::string objPath);
 
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+//{
+//    glViewport(0, 0, width, height);
+//}
+//
+//void processInput(GLFWwindow* window)
+//{
+//    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+//        glfwSetWindowShouldClose(window, true);
+//}
 
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
+std::string objPath = "resources/bobrMickBlue/bobr_turm_mick_blue.obj";
 
 int main() {
-    camera();
+    walkthemodel(objPath);
 }
 
 // needed for this project
 int WinMain() { 
-    camera(); 
+    walkthemodel(objPath);
 }
